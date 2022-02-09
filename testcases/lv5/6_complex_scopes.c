3ff8ca692ae8f00eb9781319bfec0b4a25ad4365
MaxXSoft <x@MaxXSoft.net>
2022-02-09 11:54:35 +0800

    (lv5) modified some details

--- testcases/lv5/6_complex_scopes.c
+++ testcases/lv5/6_complex_scopes.c
@@ -1,38 +1,39 @@
 int main() {
-  int a = 1, k = 0;
+  int a = 1, sum = 0;
   {
     a = a + 2;
     int b = a + 3;
     b = b + 4;
-    k = k + a + b;
+    sum = sum + a + b;
     {
       b = b + 5;
       int c = b + 6;
       a = a + c;
-      k = k + a + b + c;
+      sum = sum + a + b + c;
       {
         b = b + a;
         int a = c + 7;
         a = a + 8;
-        k = k + a + b + c;
+        sum = sum + a + b + c;
         {
           b = b + a;
           int b = c + 9;
-          const int a = 100;
-          b = b + 10;
-          k = k + a + b + c;
+          a = a + 10;
+          const int a = 11;
+          b = b + 12;
+          sum = sum + a + b + c;
           {
             c = c + b;
-            int c = b + 11;
+            int c = b + 13;
             c = c + a;
-            k = k + a + b + c;
+            sum = sum + a + b + c;
           }
-          k = k - c;
+          sum = sum - c;
         }
-        k = k - b;
+        sum = sum - b;
       }
-      k = k - a;
+      sum = sum - a;
     }
   }
-  return k % 77;
+  return sum % 77;
 }
