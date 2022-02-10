60c4be64072d9cf2b39286ff8099bec8679c178f
MaxXSoft <x@MaxXSoft.net>
2022-02-10 09:22:54 +0800

    (lv9) added test for global array access

--- testcases/lv9/08_arr_access.c
+++ testcases/lv9/08_arr_access.c
@@ -1,10 +1,12 @@
+int ga[10], gb[2][3] = {5, 6, 7, 8};
+
 int main() {
   int a[10], b[2][3] = {1, 2, 3, 4}, sum = 0;
   int i = 0;
   while (i < 2) {
     int j = 0;
     while (j < 3) {
-      sum += b[i][j];
+      sum = sum + b[i][j] + gb[2][3];
       j = j + 1;
     }
     i = i + 1;
@@ -12,6 +14,7 @@ int main() {
   i = 0;
   while (i < 10) {
     a[i] = sum + i;
+    ga[i] = sum + i;
     i = i + 1;
   }
   return sum;
--- testcases/lv9/09_const_arr_read.c
+++ testcases/lv9/09_const_arr_read.c
@@ -1,8 +1,10 @@
+const int garr[10] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
 int main() {
   const int arr[10] = {1, 2, 3, 4, 5};
   int i = 0, sum = 0;
   while (i < 10) {
-    sum += arr[i];
+    sum = sum + arr[i] + garr[i];
     i = i + 1;
   }
   return sum;
